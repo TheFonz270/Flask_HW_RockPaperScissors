cac743aa7e2d380d2795d223c0452f2568979f47
Martin Quinn <thefonz270@hotmail.com>
2021-11-05 13:51:18 +0000

    added basic html draft for play.html and basic get app.routes

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
+@app.route('/showdown')
+def showdown():
+    return render_template('showdown.html')
