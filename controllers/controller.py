5423d57d798368f8a4e67c1552bd9047cf1aef6a
Martin Quinn <thefonz270@hotmail.com>
2021-11-06 17:53:34 +0000

    Basic player vs player works but with cheating multi app routes

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, request
 from models.player import Player, players
+from models.game import Game
 # from models.play_test import players
 
 @app.route('/')
@@ -14,21 +15,42 @@ def play():
 @app.route('/player1', methods=['POST'])
 def player1():
     print (request.form)
-    name = request.form['name']
-    choice = request.form['choice']
-    player1 = Player(name, choice)
+    name1 = str(request.form['name'])
+    choice1 = str(request.form['choice'])
+    player1 = Player(name1, choice1)
     Player.add_player_to_players(player1)
     return redirect('/play')
 
 @app.route('/player2', methods=['POST'])
 def player2():
     print (request.form)
-    name = request.form['name']
-    choice = request.form['choice']
-    player2 = Player(name, choice)
+    name2 = str(request.form['name'])
+    choice2 = str(request.form['choice'])
+    player2 = Player(name2, choice2)
     Player.add_player_to_players(player2)
     return redirect('/play')
 
 @app.route('/showdown')
 def showdown():
     return render_template('showdown.html')
+
+# @app.route('/<choice1>/<choice2>')
+@app.route('/Rock/Rock')
+@app.route('/Rock/Paper')
+@app.route('/Rock/Scissors')
+@app.route('/Paper/Rock')
+@app.route('/Paper/Paper')
+@app.route('/Paper/Scissors')
+@app.route('/Scissors/Rock')
+@app.route('/Scissors/Paper')
+@app.route('/Scissors/Scissors')
+def play_game():    
+    game = Game(players)
+    result = game.play_game(players)
+    if result == 1:
+        loser = 0
+    elif result == 0:
+        loser = 1
+    else:
+        loser = "none"
+    return render_template('result.html', result=result, players=players, loser=loser)
--- models/game.py
+++ models/game.py
@@ -1,4 +1,14 @@
 class Game():
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self, players):
+        self.players = players
+        self.game_logic = {"Rock":{"Rock":"Tie", "Paper":1, "Scissors":0}, "Paper":{"Rock":0, "Paper":"Tie", "Scissors":1}, "Scissors":{"Rock":1, "Paper":0, "Scissors":"Tie"}}
+    
+
+    
+    def play_game(self, players):
+        winner = self.game_logic[players[0].choice][players[1].choice]
+        if winner == 1:
+            loser = 0 
+        elif winner == 0:
+            loser = 1
+        return winner
